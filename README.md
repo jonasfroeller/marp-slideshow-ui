9ce2a1b106babd78317744fd0982dc70b6be0d39
Jonas <publik.froeller.jonas@gmx.at>
2024-01-14 16:13:38 +0100

    mvp: added templ layout and home page

---
+++ README.md
@@ -0,0 +1,14 @@
+# marp-slideshow-ui
+
+Write marp slideshows in a visual web-interface.
+
+## templ to go
+
+```bash
+templ generate
+```
+## run server
+
+```bash
+go run server.go
+```
--- server.go
+++ server.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"net/http"
-	
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/a-h/templ"
+    "marp-slideshow-ui/views"
 )
+
+func render(ctx echo.Context, cmp templ.Component) error {
+	return cmp.Render(ctx.Request().Context(), ctx.Response())
+}
+
+func homeHandler(c echo.Context) error {
+	return render(c, views.Home("Home"))
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", homeHandler)
+
+	fmt.Println("Listening on localhost:1323")
 	e.Logger.Fatal(e.Start(":1323"))
 }
